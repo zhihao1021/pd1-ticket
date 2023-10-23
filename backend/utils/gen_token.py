45d016bac780ae064dd2647bfd6e956e7d7854d0
CHIH-HAO, CHUANG <contact@chih-hao.xyz>
2023-10-23 12:07:40 +0800

    Auto Commit

--- backend/utils/gen_token.py
+++ backend/utils/gen_token.py
@@ -1,15 +1,4 @@
-from hashlib import sha256
 from os import urandom
-
-from config import ADMIN_TOKEN
 
 def gen_token():
     return urandom(16).hex()
-
-def hash_token(token: str, ip: str):
-    if token == ADMIN_TOKEN:
-        return token
-    sha = sha256(token.encode())
-    sha.update(ip.encode())
-    hash_text = sha.hexdigest()
-    return hash_text[:32]

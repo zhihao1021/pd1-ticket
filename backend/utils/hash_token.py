db1d2778f81be42a85b154e3b5f894b36ffbc257
CHIH-HAO, CHUANG <contact@chih-hao.xyz>
2023-10-25 00:41:37 +0800

    Auto Commit

--- backend/utils/hash_token.py
+++ backend/utils/hash_token.py
@@ -6,6 +6,6 @@ def hash_token(token: str, ip: str):
     if token == ADMIN_TOKEN:
         return token
     sha = sha256(token.encode())
-    sha.update(ip.encode())
+    # sha.update(ip.encode())
     hash_text = sha.hexdigest()
     return hash_text[:32]

9f5908a14219c2b57a57191cf666e53fb1fa2a9b
CHIH-HAO, CHUANG <contact@chih-hao.xyz>
2023-12-07 20:55:55 +0800

    Fix bug

--- backend/api/routers/pull.py
+++ backend/api/routers/pull.py
@@ -90,12 +90,13 @@ async def explorer_websocket(
         path = await ws.receive_text()
         path = await sftp.realpath(path)
         if not await sftp.isdir(path):
-            path = await sftp.realpath(f"~")
+            path = await sftp.realpath("~")
         while True:
             try:
                 result = await listdir(sftp, uid, path)
             except:
                 path = await sftp.realpath(f"~")
+                print(path)
                 result = await listdir(sftp, uid, path)
             await ws.send_json(result.model_dump())
             try:
